ca37695dea8a9eeca76cdcad20ccf12eb9943a58
delmisonseidel <delmison15@gmail.com>
2021-11-18 17:44:05 -0300

    atualizando o Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import QRCode from 'react-native-qrcode';
+import QRCode from 'react-native-qrcode-svg';
+import  Icon  from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Code } from './styles';
+import { Container, Code, Nav, NavItem, NavText } from './styles';
 
 export default function Menu() {
     return (
@@ -11,9 +12,27 @@ export default function Menu() {
                 <QRCode 
                   value="https://www.rocketseat.com.br"
                   size={80}
-                  bgColor="#FFF"
-                  fgColor="#8B10AE"
+                  color="#FFF"
+                  backgroundColor="#8B10AE"
                 />
+                <Nav>
+                    <NavItem>
+                        <Icon name="help-outline" size={20} color="#FFF" />
+                        <NavText>Me ajuda</NavText>
+                    </NavItem>
+                    <NavItem>
+                        <Icon name="person-outline" size={20} color="#FFF" />
+                        <NavText>Perfil</NavText>
+                    </NavItem>
+                    <NavItem>
+                        <Icon name="credit-card" size={20} color="#FFF" />
+                        <NavText>Configurar cartão</NavText>
+                    </NavItem>
+                    <NavItem>
+                        <Icon name="smartphone" size={20} color="#FFF" />
+                        <NavText>Configurações do app</NavText>
+                    </NavItem>
+                </Nav>
             </Code>
         </Container>
     );
